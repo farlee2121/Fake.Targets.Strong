2edf4f6088c5a750ae8dcb2f9197ae19587c7b0b
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2023-03-26 19:31:39 -0500

    Demonstrate a very basic fake build project

---
+++ build.fs
@@ -0,0 +1,24 @@
+﻿module Build
+
+open Fake.Core
+
+//Context.setExecutionContext false ""() |> ignore
+
+let initTargets () =
+    Target.create "Hello" (fun _ ->
+        printfn "hello from FAKE!"
+  
+    )   
+
+[<EntryPoint>]
+let main argv =
+    argv
+    |> Array.toList
+    |> Context.FakeExecutionContext.Create false "build.fsx"
+    |> Context.RuntimeContext.Fake
+    |> Context.setExecutionContext
+
+    initTargets ()
+
+    Target.runOrDefault "Hello"
+    0
